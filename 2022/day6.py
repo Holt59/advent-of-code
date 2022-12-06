7cb831765918c351ba4c0f12f8fa9f18b09dd5c8
Mikael CAPELLE <mikael.capelle@thalesaleniaspace.com>
2022-12-06 09:03:22 +0100

    Add day 6.

---
+++ 2022/day6.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+import itertools
+import sys
+
+data = sys.stdin.read().strip()
+
+
+def index_of_first_n_differents(data: str, n: int) -> int:
+    for i in range(len(data)):
+        if len(set(data[i : i + n])) == n:
+            return i + n
+    return -1
+
+
+print(f"answer 1 is {index_of_first_n_differents(data, 4)}")
+print(f"answer 2 is {index_of_first_n_differents(data, 14)}")
